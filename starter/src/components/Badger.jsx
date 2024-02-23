7ea90481bf75af0d4e04f2beba1a2a5cf0e87095
hbattist <hbattist@wisc.edu>
2024-02-23 11:39:20 -0600

    changes

--- starter/src/components/Badger.jsx
+++ starter/src/components/Badger.jsx
@@ -1,8 +1,21 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import { useState } from "react";
 
 function Badger(props) {
+    const [isHovering, setIsHovering] = useState(false)
+    
+    function sayName() {
+        alert("I am Bucky!")
+    }
     return <Card style={{margin: "0.5rem"}}>
         <h2>Bucky Badger</h2>
+        <p>bucky.badger@wisc.edu</p>
+        <Button
+        variant="success"
+        onClick={sayName}
+        onMouseOver={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+        >Say Name</Button>
     </Card>
 }
 
